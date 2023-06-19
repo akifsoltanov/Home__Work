db723f373d3a6f20561f00ef9587bd1e23350880
Akif Soltanov <akif.soltanov12@gmail.com>
2023-06-19 23:53:20 +0400

    Tasks

---
+++ Array metods/main.js
@@ -0,0 +1,26 @@
+function getTime(millisecond) {
+
+    let time, year, month, week, day, hours, min, sec = 0;
+
+
+    sec = parseInt(millisecond / 1000);
+
+
+    min = parseInt(sec / 60);
+
+    hours = parseInt(min / 60);
+
+
+    day = parseInt(hours / 24);
+
+
+    Week - parseInt(day / 7);
+    month = parseInt(day / 30);
+
+    yearsparseInt(month / 12);
+
+    time = `${year} year ${month} month ${week} week ${day} day ${hours} hours ${min} minute ${sec} second`
+
+    return time;
+}
+console.log(getTime(1000000))
---
+++ JS DOM part-1/main.js
@@ -0,0 +1,46 @@
+// Task 1:
+
+// let body = document.querySelector('body');
+// let title = document.querySelector('.title');
+// let text = document.querySelector('p');
+// let btn = document.querySelector('#btn');
+
+// console.log(body)
+// console.log(title)
+// console.log(text)
+// console.log(btn)
+
+// btn.addEventListener("click", function () {
+//     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//     document.body.style.backgroundColor = '#' + randomColor;
+// })
+
+
+
+// Task 2:
+
+// let btn = document.querySelector('#btn');
+// let set = document.querySelector('.set')
+// console.log(btn);
+
+// let count = 0;
+
+// btn.addEventListener('click', () => {
+//     count++;
+//     updateDisplay();
+// })
+
+// function updateDisplay() {
+//     set.innerHTML = count;
+// }
+
+
+
+// Task 3:
+
+// let btn = document.querySelector('.btn')
+// let img = document.querySelector('.img')
+
+// btn.addEventListener('click', () => {
+//     img.setAttribute('src', 'imagess')
+// })
